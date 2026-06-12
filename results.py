bd6e695153dff3efb8d618810960f3e01a928136
ellpepper <elliotbourdais@gmail.com>
2026-06-12 14:22:50 +0100

    Create results.py

---
+++ results.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import gspread
+import json
+import os
+from google.oauth2.service_account import Credentials
+
+SHEET_NAME = "2026 World Cup"
+TAB_NAME = "Automated Results"
+
+# Load credentials from GitHub secret
+creds_json = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+scopes = ["https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_info(creds_json, scopes=scopes)
+client = gspread.authorize(creds)
+
+# Fetch data
+url = "https://raw.githubusercontent.com/upbound-web/worldcup-live.json/master/2026/worldcup.json"
+data = requests.get(url).json()
+df = pd.DataFrame(data['matches'])
+
+# Write to Google Sheets
+sheet = client.open(SHEET_NAME).worksheet(TAB_NAME)
+sheet.clear()
+sheet.update([df.columns.tolist()] + df.values.tolist())
+
+print("Results updated successfully")
